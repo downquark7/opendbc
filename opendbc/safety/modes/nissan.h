37c1d9e921d0c5dc7226ac5e3611783d08aa6c25
downquark7 <mail2emn@gmail.com>
2025-10-15 16:25:33 -0500

    bring 2016 altima closer to pre-changes state

--- opendbc/safety/modes/nissan.h
+++ opendbc/safety/modes/nissan.h
@@ -134,8 +134,8 @@ static safety_config nissan_init(uint16_t param) {
 
   static const CanMsg NISSAN_TX_MSGS_ALTIMA[] = {
     {0x169, 0, 8, .check_relay = true},   // LKAS
-    {0x2b1, 0, 8, .check_relay = false},  // PROPILOT_HUD (not TXed on Altima)
-    {0x4cc, 0, 8, .check_relay = false},  // PROPILOT_HUD_INFO_MSG (not TXed on Altima)
+    {0x2b1, 0, 8, .check_relay = true},  // PROPILOT_HUD
+    {0x4cc, 0, 8, .check_relay = true},  // PROPILOT_HUD_INFO_MSG
     {0x20b, 1, 6, .check_relay = false}   // CRUISE_THROTTLE (Altima on non-relayed bus)
   };
 
